6dc2e977fdbd57daa9f0c1f0661667c094650d90
Ryan Miller <ryanmillergm@gmail.com>
2019-04-24 09:37:42 -0600

    Adds error html page

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,9 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_gets_error_for_nonexisting_page
+    visit '/blog'
+
+    assert_equal 404, page.status_code
 end
